b3ca63e16022c5b167d2507c0fe578bde0ad3584
ryshino <ranature1153@gmail.com>
2020-12-06 07:30:57 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = "タスクを追加しました"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクが追加できませんでした"
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = "タスクを編集しました"
+      redirect_to @task
+    else
+      flash[:danger] = "タスクが編集できませんでした"
+      render :new
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = "タスクを削除しました"
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
